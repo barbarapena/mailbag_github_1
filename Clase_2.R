00ee5b4a801261973150e09caf34a8c7547b6735
barbarapena <bpena1@uc.cl>
2020-08-22 13:30:03 -0400

    He agregado más instrucciones

--- Clase_2.R
+++ Clase_2.R
@@ -9,4 +9,13 @@ library(kableExtra)
 library(knitr)
 library(tidyverse)
 
+library(git)
+#Esto en la terminal de Rstudio(pestaña al lado de la consola)
+git config --global user.email "bpena1@uc.cl"
+git config --global user.name "barbarapena"
 
+
+#importar los datos de hoy
+plants <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/plants.csv')
+actions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/actions.csv')
+threats <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/threats.csv') 
