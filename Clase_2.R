211a06c4715f8a8c1ca20f5db2901b0bdb1c6a48
barbarapena <bpena1@uc.cl>
2020-08-22 12:37:54 -0400

    Codigo inicial clase 2

---
+++ Clase_2.R
@@ -0,0 +1,12 @@
+#clase 2 Dereck 18. agosto.2020
+# Investigación Reproducible. 
+
+install.packages("rmarkdown")
+install.packages("kableExtra")
+install.packages("knitr")
+library(rmarkdown)
+library(kableExtra)
+library(knitr)
+library(tidyverse)
+
+
